d5a1329e52b5051253f7c9a522b4ce6dcbcd587c
Giovanny <giovanny.hernandez@blackboard.com>
2021-08-20 12:43:58 -0500

    Reword message notification on pending request

--- lambda/add_meaning.py
+++ lambda/add_meaning.py
@@ -290,7 +290,7 @@ def notify_pending_approval(user_id, acronym):
                         "type": "section",
                         "text": {
                             "type": "mrkdwn",
-                            "text": f"*Submission successful!*\n We received your submission for *{acronym}*.\nPlease check @define_bot for updates."
+                            "text": f"*Submission successful!*\n We received your submission for *{acronym}*.\nAs soon as we approve it you should see it available."
                         }
                     }
                 ]
