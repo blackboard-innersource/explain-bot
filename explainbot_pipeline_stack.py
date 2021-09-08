98f90a84dc99240405df817472efd51f907255f9
jcalero23 <jhonier.calero@blackboard.com>
2021-09-08 11:57:36 -0500

    Change trigger branch

--- explainbot_pipeline_stack.py
+++ explainbot_pipeline_stack.py
@@ -28,7 +28,7 @@ class ExplainSlackBotPipelineStack(cdk.Stack):
                 oauth_token=cdk.SecretValue.secrets_manager('GITHUB_TOKEN_NAME'),
                 owner='blackboard-innersource',
                 repo='explain-bot',
-                branch='feature/persist-denied-acronyms',
+                branch='develop',
                 trigger=cpactions.GitHubTrigger.POLL
             ),
             synth_action=SimpleSynthAction(
